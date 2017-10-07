ca4693f7b713300c28e9c4f58b63f20af6b905ef
David Baker <dave@matrix.org>
2017-10-07 14:37:47 +0200

    Add priority level

--- src/components/AnnouncePoster.js
+++ src/components/AnnouncePoster.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
+const INITIAL_FORM_VALS = {
+    announceText: '',
+    level: 'info',
+};
+
 export default class AnnouncePoster extends React.Component {
     constructor() {
         super();
 
         this.onSubmit = this.onSubmit.bind(this);
         this.onAnnounceTextChange = this.onAnnounceTextChange.bind(this);
+        this.onLevelChange = this.onLevelChange.bind(this);
 
         this.state = {
-            form: {
-                announceText: '',
-            },
+            form: {},
         };
+        Object.assign(this.state.form, INITIAL_FORM_VALS);
     }
 
     onSubmit(ev) {
@@ -20,9 +25,18 @@ export default class AnnouncePoster extends React.Component {
         return this.props.app.matrixClient.sendEvent(
             this.props.app.roomId,
             'm.room.message',//"x.announcement",
-            {msgtype: 'm.text', body: this.state.form.announceText},
+            {
+                msgtype: 'm.text',
+                body: this.state.form.announceText,
+                level: this.state.form.level,
+            },
         ).then(() => {
             console.log("done");
+            const formVals = {};
+            Object.assign(formVals, INITIAL_FORM_VALS);
+            this.setState({
+                form: formVals,
+            });
         });
     }
 
@@ -30,6 +44,15 @@ export default class AnnouncePoster extends React.Component {
         const newVal = e.target.value;
         this.setState((s) => {
             s.form.announceText = newVal;
+            return s;
+        });
+    }
+
+    onLevelChange(e) {
+        const newVal = e.target.value;
+        this.setState((s) => {
+            s.form.level = newVal;
+            return s;
         });
     }
 
@@ -43,6 +66,11 @@ export default class AnnouncePoster extends React.Component {
                     onChange={this.onAnnounceTextChange}
                 />
                 <br />
+                <select value={this.state.form.level} onChange={this.onLevelChange}>
+                    <option value="info">Info</option>
+                    <option value="warning">Warning</option>
+                    <option value="emergency">Emergency</option>
+                </select>
                 <input type="submit" />
             </form>
         </div>;
